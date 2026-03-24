0baabc19d706a3f97ffb2cbfb66bf5d483d31136
Göran Sander <goran.sander@gmail.com>
2026-03-24 20:17:40 +0100

    chore(pre-release/rc): release helpbutton-qs 2.6.0

--- variants/bug-report/de/helpbutton-qs.js
+++ variants/bug-report/de/helpbutton-qs.js
@@ -23,7 +23,7 @@
  *
  * Compatible with Qlik Sense Enterprise on Windows (client-managed).
  *
- * @version 2.5.3 // x-release-please-version
+ * @version 2.6.0 // x-release-please-version
  * @license MIT
  * @see https://github.com/ptarmiganlabs/help-button.qs
  */
